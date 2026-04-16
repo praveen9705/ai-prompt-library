1a05fecd2d38cbcb79e9681e60803d8a510d089c
praveen9705 <praveenkumarchittimala21@gmail.com>
2026-04-16 23:29:24 +0530

    Connected frontend to live backend

--- frontend/src/app/services/prompt.service.ts
+++ frontend/src/app/services/prompt.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PromptService {
 
-  apiUrl = 'http://127.0.0.1:8000/prompts/';
+  apiUrl = 'https://ai-prompt-backend-8x87.onrender.com/prompts/';;
 
   constructor(private http: HttpClient) {}
 
